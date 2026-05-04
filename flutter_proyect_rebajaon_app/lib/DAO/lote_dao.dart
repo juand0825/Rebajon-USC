0099dc2ab060d3f8cb839f14a48d345c4422f9f7
Juan Diego <juangonzalezj0825@gmail.com>
2026-05-04 12:28:41 -0500

    modifi

---
+++ flutter_proyect_rebajaon_app/lib/DAO/lote_dao.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_proyect_rebajaon_app/models/lote_model.dart';
+
+import '../database/db_connection.dart';
+import '../Models/lote_model.dart';
+
+class LoteDao {
+  Future<void> insertarLote(LoteModel lote) async {
+    final conn = await DbConnection.getConnection();
+
+    await conn.execute(
+      '''
+      INSERT INTO lotes
+      (id_medicamento, numero_lote, fecha_fabricacion, fecha_vencimiento, cantidad_inicial, cantidad_disponible, codigo_barras)
+      VALUES (:id_medicamento, :numero_lote, :fecha_fabricacion, :fecha_vencimiento, :cantidad_inicial, :cantidad_disponible, :codigo_barras)
+      ''',
+      {
+        'id_medicamento': lote.idMedicamento,
+        'numero_lote': lote.numeroLote,
+        'fecha_fabricacion': lote.fechaFabricacion,
+        'fecha_vencimiento': lote.fechaVencimiento,
+        'cantidad_inicial': lote.cantidadInicial,
+        'cantidad_disponible': lote.cantidadDisponible,
+        'codigo_barras': lote.codigoBarras,
+      },
+    );
+  }
+
+  Future<List<LoteModel>> listarLotesPorMedicamento(int idMedicamento) async {
+    final conn = await DbConnection.getConnection();
+
+    final result = await conn.execute(
+      'SELECT * FROM lotes WHERE id_medicamento = :id_medicamento',
+      {'id_medicamento': idMedicamento},
+    );
+
+    return result.rows
+        .map((row) => LoteModel.fromMap(row.assoc()))
+        .toList();
+  }
+}
--- flutter_proyect_rebajaon_app/lib/DAO/medicamento_dao.dart
+++ flutter_proyect_rebajaon_app/lib/DAO/medicamento_dao.dart
@@ -1,1 +1,38 @@
-import 'package:flutter/material.dart';
+import 'package:flutter_proyect_rebajaon_app/models/medicamento_model.dart';
+
+import '../database/db_connection.dart';
+import '../Models/medicamento_model.dart';
+
+class MedicamentoDao {
+  Future<void> insertarMedicamento(MedicamentoModel medicamento) async {
+    final conn = await DbConnection.getConnection();
+
+    await conn.execute(
+      '''
+      INSERT INTO medicamentos
+      (nombre, principio_activo, presentacion, fabricante, stock_actual, stock_minimo, requiere_refrigeracion, activo)
+      VALUES (:nombre, :principio_activo, :presentacion, :fabricante, :stock_actual, :stock_minimo, :requiere_refrigeracion, :activo)
+      ''',
+      {
+        'nombre': medicamento.nombre,
+        'principio_activo': medicamento.principioActivo,
+        'presentacion': medicamento.presentacion,
+        'fabricante': medicamento.fabricante,
+        'stock_actual': medicamento.stockActual,
+        'stock_minimo': medicamento.stockMinimo,
+        'requiere_refrigeracion': medicamento.requiereRefrigeracion ? 1 : 0,
+        'activo': medicamento.activo ? 1 : 0,
+      },
+    );
+  }
+
+  Future<List<MedicamentoModel>> listarMedicamentos() async {
+    final conn = await DbConnection.getConnection();
+
+    final result = await conn.execute('SELECT * FROM medicamentos');
+
+    return result.rows
+        .map((row) => MedicamentoModel.fromMap(row.assoc()))
+        .toList();
+  }
+}
